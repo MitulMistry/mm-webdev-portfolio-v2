586b0ce3689886615ceb3cebbadd69f2d914c4b7
MitulMistry <MitulMistryDev@gmail.com>
2017-12-18 18:05:59 -0600

    Delete and reorganize generated files

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>
+          mm-webdev-portfolio-v2
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
