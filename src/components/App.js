8ef2a3de204dccd68c82b442519cfdb136396106
MitulMistry <MitulMistryDev@gmail.com>
2017-12-22 20:19:05 -0600

    Set up router

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { MainPage } from './MainPage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MainPage />
+        <Router>
+          <Switch>
+            <Route path="/" component={MainPage} />
+          </Switch>
+        </Router>
       </div>
     );
   }
