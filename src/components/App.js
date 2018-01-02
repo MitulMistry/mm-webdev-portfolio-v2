74a5eb4cc4c9a79f009b8f68698dd73b444ee652
MitulMistry <MitulMistryDev@gmail.com>
2018-01-02 16:36:52 -0600

    Implement ReactGA for Google Analytics in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 import { MainPage } from './MainPage';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-6842596-2'); //Unique Google Analytics tracking number
+
+const history = createBrowserHistory();
+
+history.listen((location, action) => { //fire Google Analytics on route change
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+});
 
 class App extends Component {
+  componentWillMount() { //fire Google Analytics when page first loads
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+  }
+
   render() {
     return (
       <div className="App">
-        <Router>
+        <Router history={history}>
           <Switch>
             <Route path="/" component={MainPage} />
           </Switch>
