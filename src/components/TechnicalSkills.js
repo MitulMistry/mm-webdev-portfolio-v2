877eee97bd35c0332584c7642c97169b059f3eb2
MitulMistry <MitulMistryDev@gmail.com>
2018-03-30 00:47:01 -0500

    Update technical skills

--- src/components/TechnicalSkills.js
+++ src/components/TechnicalSkills.js
@@ -6,9 +6,9 @@ export class TechnicalSkills extends React.Component {
       <div>
         <h1>Technical Skills</h1>
         <p><strong>Languages:</strong> Ruby, JavaScript, HTML/CSS</p>
-        <p><strong>Frameworks:</strong> Ruby on Rails, AngularJS</p>
+        <p><strong>Frameworks:</strong> Ruby on Rails, AngularJS, React</p>
         <p><strong>Databases:</strong> SQLite</p>
-        <p><strong>Tools:</strong> jQuery, ActiveRecord, Git, Github, Bootstrap, Heroku</p>
+        <p><strong>Tools:</strong> jQuery, ActiveRecord, Git, Github, Bootstrap, Heroku, Webpack</p>
         <p><strong>Concepts:</strong> Object-oriented programming, AJAX, RESTful architecture / APIs</p>
       </div>
     );
