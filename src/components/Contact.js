03088371af9723d2697bd916f8d68b3702f18aa4
MitulMistry <MitulMistryDev@gmail.com>
2018-04-05 15:41:46 -0500

    Restyle and update contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,38 @@
 import React from 'react';
+import { Row, Col, Button } from 'reactstrap';
 
 export class Contact extends React.Component {
   render() {
     return (
-      <div>        
-        <p className="contact"><a href="mailto:MitulMistryDev@gmail.com">MitulMistryDev@gmail.com</a></p>
-        <p className="contact-icons">
-          <a href="https://github.com/MitulMistry" target="_blank" rel="noopener noreferrer"><span className="fa fa-github fa-lg"></span></a>
-          <a href="https://www.linkedin.com/in/mitulmistry" target="_blank" rel="noopener noreferrer"><span className="fa fa-linkedin fa-lg"></span></a>
-          <a href="https://twitter.com/mmystic" target="_blank" rel="noopener noreferrer"><span className="fa fa-twitter fa-lg"></span></a>
-          <a href="mailto:MitulMistryDev@gmail.com" rel="noopener noreferrer"><span className="fa fa-envelope-o fa-lg"></span></a>
-          <a href="http://mitulmistry.github.io/" target="_blank" rel="noopener noreferrer">Blog</a>
-        </p>
+      <div className="contact">
+        <Row>
+          <Col md="2">
+            <Button href="mailto:MitulMistryDev@gmail.com" className="btn-web-app btn-light" rel="noopener noreferrer"><span className="fa fa-envelope-o fa-lg" aria-hidden="true"></span> Email</Button>
+          </Col>
+          <Col>
+            <p>Email me at:</p>
+            <p><a href="mailto:MitulMistryDev@gmail.com" rel="noopener noreferrer">MitulMistryDev@gmail.com</a></p>
+          </Col>
+        </Row>
+        <Row className="row-top-margin">
+        <Col md="2">
+            <Button href="https://www.linkedin.com/in/mitulmistry" target="_blank" className="btn-web-app btn-light" rel="noopener noreferrer"><span className="fa fa-linkedin fa-lg" aria-hidden="true"></span> LinkedIn</Button>
+          </Col>
+          <Col>
+            <p>Or add me on LinkedIn at:</p>
+            <p><a href="https://www.linkedin.com/in/mitulmistry" target="_blank" rel="noopener noreferrer">LinkedIn.com/in/MitulMistry</a></p>
+          </Col>
+        </Row>
       </div>
     );
   }
 }
+
+{/* <p className="contact"><a href="mailto:MitulMistryDev@gmail.com">MitulMistryDev@gmail.com</a></p>
+  <p className="contact-icons">
+    <a href="https://github.com/MitulMistry" target="_blank" rel="noopener noreferrer"><span className="fa fa-github fa-lg"></span></a>
+    <a href="https://www.linkedin.com/in/mitulmistry" target="_blank" rel="noopener noreferrer"><span className="fa fa-linkedin fa-lg"></span></a>
+    <a href="https://twitter.com/mmystic" target="_blank" rel="noopener noreferrer"><span className="fa fa-twitter fa-lg"></span></a>
+    <a href="mailto:MitulMistryDev@gmail.com" rel="noopener noreferrer"><span className="fa fa-envelope-o fa-lg"></span></a>
+    <a href="http://mitulmistry.github.io/" target="_blank" rel="noopener noreferrer">Blog</a>
+  </p> */}
