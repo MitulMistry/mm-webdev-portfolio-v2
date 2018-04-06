100db3ba9888e1dee85ce2744eb7a9873f32dc97
MitulMistry <MitulMistryDev@gmail.com>
2018-04-06 18:22:30 -0500

    Add social icons to right side of navbar

--- src/components/MainNavBar.js
+++ src/components/MainNavBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Container, Navbar, Nav, NavItem, NavLink } from 'reactstrap';
 
+import { profileData } from '../data';
+
 export class MainNavBar extends React.Component {
   render() {
     return (
         <Navbar id="main-navbar" className="sticky-top" color="light" light expand="sm">
           <Container>
-            <Nav navbar>
+            <Nav navbar className="mr-auto">
               <NavItem>
                 <NavLink href="#about">About</NavLink>
               </NavItem>
@@ -15,6 +17,21 @@ export class MainNavBar extends React.Component {
               </NavItem>
               <NavItem>
                 <NavLink href="#contact">Contact</NavLink>
+              </NavItem>
+            </Nav>
+
+            <Nav navbar className="ml-auto navbar-social">
+              <NavItem>
+                <NavLink href={profileData.github} target="_blank"><span className="fa fa-github fa-lg"></span></NavLink>                
+              </NavItem>
+              <NavItem>
+                <NavLink href={profileData.linkedin} target="_blank"><span className="fa fa-linkedin fa-lg"></span></NavLink>                
+              </NavItem>
+              <NavItem>
+                <NavLink href={profileData.twitter} target="_blank"><span className="fa fa-twitter fa-lg"></span></NavLink>                
+              </NavItem>
+              <NavItem>
+                <NavLink href={"mailto:" + profileData.email} target="_blank"><span className="fa fa-envelope-o fa-lg"></span></NavLink>                
               </NavItem>
             </Nav>
           </Container>
