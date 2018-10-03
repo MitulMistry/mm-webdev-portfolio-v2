8ab2e4e1ea7ff011477f5f26691e3be44fb784e4
MitulMistry <MitulMistryDev@gmail.com>
2018-10-03 00:45:15 -0500

    Correct typo

--- src/components/TechnicalSkills.js
+++ src/components/TechnicalSkills.js
@@ -5,10 +5,10 @@ export class TechnicalSkills extends React.Component {
     return (
       <div>        
         <p><strong>Languages:</strong> Ruby, JavaScript, HTML/CSS, Sass</p>
-        <p><strong>Frameworks:</strong> Ruby on Rails, AngularJS, React</p>
+        <p><strong>Frameworks:</strong> Ruby on Rails, React, AngularJS</p>
         <p><strong>Databases:</strong> SQLite</p>
         <p><strong>Tools:</strong> jQuery, ActiveRecord, Git, Github, Bootstrap, Heroku, Webpack</p>
-        <p><strong>Concepts:</strong> Object-oriented ,programming, AJAX, RESTful architecture / APIs</p>
+        <p><strong>Concepts:</strong> Object-oriented programming, AJAX, RESTful architecture / APIs</p>
         <p><strong>Software:</strong> Linux, Photoshop, Illustrator</p>
       </div>
     );
